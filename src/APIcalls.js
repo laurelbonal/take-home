d845af4101ae8d7be92223d9212537d787a2d559
laurel Bonal <laurelbonal@gmail.com>
2024-07-07 19:50:13 -0600

    Remove API key from repo

--- src/APIcalls.js
+++ src/APIcalls.js
@@ -1,13 +1,13 @@
-const API_KEY = '7ce628d456044e1085d5f92a0b0589f4';
+const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 const BASE_URL = 'https://newsapi.org/v2';
 
 export const fetchTopHeadlines = async (country = 'us') => {
-    try {
-      const response = await fetch(`${BASE_URL}/top-headlines?country=${country}&apiKey=${API_KEY}`);
-      const data = await response.json();
-      return data.articles;
-    } catch (error) {
-      console.error('Error fetching top headlines:', error);
-      return [];
-    }
-  };
+  try {
+    const response = await fetch(`${BASE_URL}/top-headlines?country=${country}&apiKey=${API_KEY}`);
+    const data = await response.json();
+    return data.articles;
+  } catch (error) {
+    console.error('Error fetching top headlines:', error);
+    return [];
+  }
+};
